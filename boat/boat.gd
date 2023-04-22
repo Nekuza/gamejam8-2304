e3f0c91ef2cd1f7ea0d0eb925ec2f61b87336bec
Nekuza <31394295+Nekuza@users.noreply.github.com>
2023-04-22 11:03:17 +0200

    boat no self spawn
    boat no longer spawns by itself. its rather placed on the map

--- boat/boat.gd
+++ boat/boat.gd
@@ -1,18 +1,15 @@
 extends RigidBody2D
 
+var start_health = 100
+var health
 signal hit
 
 # TODO
 # stats
-var health = 100
-start_position = Vector2(500,1000) 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = set_position(start_position)
-	start_health = 100 # TODO make variable for difficulty settings
-	
-	# TODO: 
+	health = start_health
 	
 	pass # Replace with function body.
 
@@ -20,17 +17,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-
-func set_position(x,y):
-	position = Vector2(x,y)
-
-func start(pos):
-	position = set_position(start_position)
-	health = start_health
-	show()
-	$CollisionShape2D.disabled = false
 
 func _on_body_entered(body):
 	health -= 1
