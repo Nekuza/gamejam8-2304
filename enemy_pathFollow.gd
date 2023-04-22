abb2851e543f522f6115525aff7251d4f4c76ed0
Nekuza <31394295+Nekuza@users.noreply.github.com>
2023-04-22 22:11:34 +0200

    working path movement
    duplicates with mob

---
+++ enemy_pathFollow.gd
@@ -0,0 +1,12 @@
+extends PathFollow2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	progress_ratio = progress_ratio + $enemy1.speed * delta
+	pass
