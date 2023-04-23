f5b3d1b71ddbf35473fa6b2603fb8f7fa2ecbef1
JPS <Schenckp@students.uni-marburg.de>
2023-04-23 13:54:02 +0200

    cleanup code

--- enemy_spawn/enemy_spawn.gd
+++ enemy_spawn/enemy_spawn.gd
@@ -4,15 +4,6 @@ signal ark_hit_1
 signal ark_hit_2
 signal ark_hit_3
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
 func spawn(enemy_type: PathFollow2D, amount: int, mob_path: Path2D):
 	for i in amount:
 		mob_path.rotation = 0
@@ -23,11 +14,8 @@ func spawn(enemy_type: PathFollow2D, amount: int, mob_path: Path2D):
 			enemy_type.ark_hit.connect(_on_path_2_ark_hit)
 		else:
 			enemy_type.ark_hit.connect(_on_path_1_ark_hit)
-		# TODO: missing hit -> fix signal
 		mob_path.add_child(enemy_type)
 		$StaticBody2D/AnimatedSprite2D.play("open_gate")
-	#TODO implement spawn
-	pass
 
 func _on_path_1_ark_hit():
 	ark_hit_1.emit()
